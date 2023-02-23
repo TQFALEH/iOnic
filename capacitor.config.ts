7ea5e70b33d38139dcbd388d9d91770604a85143
tqfaleh <rescco14@gmail.com>
2023-02-23 14:51:39 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tq.test',
+  appName: 'tq-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
